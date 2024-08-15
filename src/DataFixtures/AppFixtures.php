c278162ce09b47e8deaaa289390304d76ed26306
Benjamin Schacher <benjamin.schacher@laposte.net>
2024-08-15 16:40:44 +0200

    fixture pour toute la bdd

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,56 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\CardList;
+use App\Entity\Deck;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+
+    private const USERS_NAMES = ["Jhon", "Benrnard", "Tony", "Ben"];
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create('fr_FR');
+        $decklist = [];
+        $userlist = [];
+
+        // --- User ----------------------------------------------
+        foreach (self::USERS_NAMES as $UserName) {
+            $user = new User();
+            $user->setName($UserName);
+            $user->setPassword($faker->password());
+            $userlist[] = $user;
+            $manager->persist($user);
+        }
+        // --- Deck ----------------------------------------------
+        foreach ($userlist as $user) {
+            $i = 0;
+            while ($i != 3) {
+                $deck = new Deck();
+                $deck->setName($faker->name());
+                $deck->setCommanderName($faker->name());
+                $deck->setCreator($user);
+                $deck->setImageName($faker->name());
+                $decklist[] = $deck;
+                $manager->persist($deck);
+                $i++;
+            }
+        }        
+        // --- Deck ----------------------------------------------
+        foreach ($decklist as $deck) {
+            $i = 0;
+            while ($i != 3) {
+                $card = new CardList();                
+                $card->setName($faker->name());
+                $card->setAddTo($deck);
+                $manager->persist($card);
+                $i++;
+            }
+        }
 
         $manager->flush();
     }
